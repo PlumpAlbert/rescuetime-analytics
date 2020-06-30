9b7c6b8c444b958135a6050e3f739106c095e278
plump_albert <plumpalbert@gmail.com>
2020-06-30 12:29:42 +0300

    Rewrote script from the ground up

---
+++ main.py
@@ -0,0 +1,133 @@
+import argparse
+from os import path, linesep
+from platform import system
+from rescuetime import analytic_data
+from datetime import date
+from colorama import Fore, Back, Style
+from csv import DictReader
+
+parser = argparse.ArgumentParser(
+    description='RescueTime Analytic API from your command line')
+# PARSER ARGUMENTS
+parser.add_argument('-k', metavar='KEY', type=str,
+                    help="Path to the file with user's API key")
+parser.add_argument('-f', '--from', nargs=1, dest='rb', metavar='YYYY-MM-DD', type=str,
+                    help="Sets the start day for data batch, inclusive.\
+                    (always at time 00:00, start hour/minute not supported)")
+parser.add_argument('-t', '--to', nargs=1, dest='re', metavar='YYYY-MM-DD', type=str,
+                    help="Sets the end day for data batch, inclusive.\
+                    (always at time 00:00, start hour/minute not supported)")
+parser.add_argument('-w', '--wage', dest='wage', type=float,
+                    help="Variable to calculate wage from productive time")
+parser.add_argument('--multiplier', type=float,
+                    help="Number to multiply all productive time")
+###
+args = dict(parser.parse_args()._get_kwargs())
+if args['k']:
+    if not path.exists(args['k']):
+        raise FileNotFoundError("\"%s\" : File does not exists" % (args['k']))
+    apikey = open(args['k'], 'r').read().replace(linesep, '')
+elif system() == "Windows":
+    apikey = open(
+        path.expanduser('~/.rescuetime'),
+        'r'
+    ).read().replace(linesep, '')
+else:
+    apikey = open(
+        path.expanduser('~/.local/share/rescuetime.key'),
+        'r'
+    ).read().replace(linesep, '')
+
+if not apikey:
+    raise Exception("No API key provided")
+
+today = date.today()
+start_of_the_month = date(today.year, today.month, 1)
+
+data = analytic_data(apikey, **{
+    'pv': 'interval',
+    'rs': 'day',
+    'rb': str(args['rb'] or start_of_the_month),
+    're': str(args['re'] or today),
+    'rk': 'productivity',
+    'format': 'csv'
+})
+
+r = DictReader(data.splitlines())
+day_info = {}
+for row in r:
+    label = row['Date'].split('T')[0]
+    if label not in day_info:
+        day_info[label] = {}
+    _prop = ''
+    if int(row['Productivity']) == 2:
+        _prop = 'VP'
+    elif int(row['Productivity']) == 1:
+        _prop = 'P'
+    elif int(row['Productivity']) == 0:
+        _prop = 'N'
+    elif int(row['Productivity']) == -1:
+        _prop = 'D'
+    elif int(row['Productivity']) == -2:
+        _prop = 'VD'
+    day_info[label][_prop] = int(row['Time Spent (seconds)'])
+# Fill empty productivity categories with 0
+for day in day_info:
+    for p in ['VP', 'P', 'N', 'D', 'VD']:
+        if p not in day_info[day]:
+            day_info[day][p] = 0
+
+
+def print_day(day, **kwargs):
+    text = day.center(12, ' ')
+    print(Fore.GREEN + text + Style.RESET_ALL, end='')
+    for key in ['VP', 'P', 'N', 'D', 'VD']:
+        if key == 'VP':
+            fc = Fore.BLUE
+        elif key == 'P':
+            fc = Fore.CYAN
+        elif key == 'N':
+            fc = Fore.WHITE
+        elif key == 'D':
+            fc = Fore.YELLOW
+        else:
+            fc = Fore.RED
+        s = kwargs[key] or 0
+        m, s = divmod(int(s), 60)
+        h, m = divmod(m, 60)
+        text = f'{h:02d}:{m:02d}'.center(7, ' ')
+        print(fc + text + Style.RESET_ALL, end='')
+    print()
+
+
+def print_table_header():
+    columns = [
+        Back.GREEN + Fore.BLACK + 'Date'.center(12, ' '),
+        Back.BLUE + Fore.BLACK + 'VP'.center(7, ' '),
+        Back.CYAN + Fore.BLACK + 'P'.center(7, ' '),
+        Back.WHITE + Fore.BLACK + 'N'.center(7, ' '),
+        Back.YELLOW + Fore.BLACK + 'D'.center(7, ' '),
+        Back.RED + Fore.BLACK + 'VD'.center(7, ' ')
+    ]
+    print(''.join(columns) + Style.RESET_ALL)
+
+
+print_table_header()
+for day in day_info:
+    print_day(day, **day_info[day])
+
+if args['wage']:
+    productive_time = 0
+    for day in day_info:
+        current_day = day_info[day]
+        if 'VP' in current_day:
+            productive_time += int(current_day['VP'])
+        if 'P' in current_day:
+            productive_time += int(current_day['P'])
+    m, s = divmod(productive_time, 60)
+    h, m = divmod(m, 60)
+    h += m/60
+    if args['multiplier']:
+        h *= args['multiplier']
+    print(f'Total productive hours: {Fore.GREEN}{h:.2f}{Style.RESET_ALL}')
+    print(f'Your wage is {Fore.GREEN}{h * args["wage"]:.2f}{Style.RESET_ALL}')
